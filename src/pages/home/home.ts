f92a68ef331db08eb467050c3ba8fc119cc017aa
David Hohl <david.hohl@gmail.com>
2017-08-19 09:33:45 +0200

    connected with directus my new headless cms

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ViewController } from 'ionic-angular';
+
+declare let require: any;
+const RemoteInstance = require('directus-sdk-javascript/remote');
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  public client;
   constructor(public navCtrl: NavController) {
+    this.client = new RemoteInstance({
+      url: 'http://backend.app-evangelist.com/api/1.1/'
+    });
+  }
 
+  ionViewDidLoad() {
+    this.client.getItems('apps')
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
   }
 
 }
